beb832f8fbefc2c4694c16c2bd098bdee1e01d81
wuchuixu <364191418@qq.com>
2022-07-20 08:45:49 +0800

    feat(base.ts)：增加isInvalid，isEmpty方法

--- src/base.ts
+++ src/base.ts
@@ -8,7 +8,7 @@ export const toRawType = (val: unknown): string => objToStr(val).slice(8, -1)
 export const isString = (val: unknown): val is string => typeof val === 'string'
 
 export const isNumber = (val: unknown): val is number => typeof val === 'number'
-             
+
 export const isFunction = (val: unknown): val is typeof Function =>
   typeof val === 'function'
 
@@ -18,3 +18,34 @@ export const isPlainObject = (val: unknown): val is object =>
   toRawType(val) === 'Object'
 
 export const isDate = (val: unknown): val is Date => val instanceof Date
+
+/**
+ * 检验某个值是否无效
+ * @param value
+ */
+export const isInvalid = (value: unknown) => {
+  return value === null || value === undefined || value === ''
+}
+
+/**
+ * 检验某个值是否空值
+ * @param value
+ */
+export const isEmpty = (value: unknown): boolean => {
+  let empty = true
+  if (isPlainObject(value)) {
+    if (Array.isArray(value)) {
+      return value.every(v => isEmpty(v))
+    } else {
+      const obj: any = value
+      Object.keys(obj).forEach(k => {
+        if (!isEmpty(obj[k])) {
+          empty = false
+        }
+      })
+    }
+  } else {
+    empty = value === null || value === undefined || value === ''
+  }
+  return empty
+}
